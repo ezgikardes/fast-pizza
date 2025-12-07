7241e21430ce39fa27cd07a08a751a8618b09c4a
ezgikardes <ezgikardes@gmail.com>
2025-12-07 21:13:01 +0100

    feat: add menu loader and update Menu component to display menu items

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./ui/Home"
-import Menu from './features/menu/Menu'
+import Menu, { loader as menuLoader } from './features/menu/Menu'
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
 import AppLayout from "./ui/AppLayout";
-
-
 
 const router = createBrowserRouter([
   {
@@ -18,7 +16,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/menu',
-        element: <Menu />
+        element: <Menu />,
+        loader: menuLoader,
       },
       {
         path: '/cart',
