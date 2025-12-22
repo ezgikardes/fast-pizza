e58318150eb4bad2b2294c8ff50358d272010ad6
ezgikardes <ezgikardes@gmail.com>
2025-12-22 19:09:38 +0100

    feat: update HTML structure and styles, add Username component to Header

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+function Username() {
+    return (
+        <div className="text-sm font-semibold">
+            Ezgi
+        </div>
+    )
+}
+
+export default Username
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import SearchOrder from "../features/order/SearchOrder"
+import Username from "../features/user/Username"
 
 function Header() {
     return (
-        <header className="bg-yellow-500">
-            <Link to='/'>Fast Pizza Co.</Link>
+        <header className="uppercase bg-yellow-500">
+            <Link to='/' className="tracking-widest">Fast Pizza Co.</Link>
             <SearchOrder />
-            <p>Ezgi</p>
+            <Username />
         </header>
     )
 }
