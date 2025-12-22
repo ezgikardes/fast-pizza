46745a238905df01d9484fd6bcb0a600cacec2b4
ezgikardes <ezgikardes@gmail.com>
2025-12-22 18:18:57 +0100

    feat: add Tailwind CSS and PostCSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "./ui/Home"
-import Menu, { loader as menuLoader } from './features/menu/Menu'
-import Cart from './features/cart/Cart';
-import CreateOrder, { action as createOrderAction } from './features/order/CreateOrder';
-import Order, { loader as orderLoader } from './features/order/Order';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error"
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
@@ -14,21 +16,21 @@ const router = createBrowserRouter([
 
     children: [
       {
-        path: '/',
-        element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: '/menu',
+        path: "/menu",
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
       {
-        path: '/cart',
-        element: <Cart />
+        path: "/cart",
+        element: <Cart />,
       },
       {
-        path: '/order/new',
+        path: "/order/new",
         element: <CreateOrder />,
         action: createOrderAction,
       },
@@ -36,14 +38,14 @@ const router = createBrowserRouter([
         path: "order/:orderId",
         element: <Order />,
         loader: orderLoader,
-        errorElement: <Error />
+        errorElement: <Error />,
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
