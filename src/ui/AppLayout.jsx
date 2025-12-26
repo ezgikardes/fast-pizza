9a8004cbe06e96d749905c7cfe449ae74be08653
ezgikardes <ezgikardes@gmail.com>
2025-12-26 19:41:07 +0100

    feat: implement grid layout to CartOverview, Header, Home, and Username components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,14 +10,16 @@ function AppLayout() {
 
 
     return (
-        <div className="layout">
+        <div className="grid grid-rows-[auto_1fr_auto] h-screen">
             {isLoading && <Loader />}
 
             <Header />
 
-            <main>
-                <Outlet />
-            </main>
+            <div className="overflow-scroll">
+                <main className="max-w-3xl mx-auto">
+                    <Outlet />
+                </main>
+            </div>
 
             <CartOverview />
         </div>
