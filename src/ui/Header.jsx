8793fb61b6a8650146a0fe004f5678909a171036
ezgikardes <ezgikardes@gmail.com>
2025-12-28 21:19:48 +0100

    feat: add Roboto Mono font and enhance Loader component styling

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username"
 
 function Header() {
     return (
-        <header className="flex items-center justify-between px-4 py-3 uppercase bg-yellow-400 border-b border-stone-200 sm:px-6">
+        <header className="flex items-center justify-between px-4 py-3 uppercase bg-yellow-400 border-b border-stone-200 sm:px-6 font-pizza">
 
             <Link to='/' className="tracking-widest">Fast Pizza Co.</Link>
             <SearchOrder />
