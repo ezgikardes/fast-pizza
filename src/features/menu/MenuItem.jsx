8185be33f93951584e381575a489e744a71a3ea1
ezgikardes <ezgikardes@gmail.com>
2026-01-04 20:35:25 +0100

    feat: add cart item functionality in MenuItem component and update Button component for onClick support

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,22 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button"
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newitem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newitem))
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -22,7 +36,12 @@ function MenuItem({ pizza }) {
                 Sold out
               </p>
             )}
-          <Button type='small'>Add to cart</Button>
+
+          {!soldOut && (
+            <Button type='small' onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
