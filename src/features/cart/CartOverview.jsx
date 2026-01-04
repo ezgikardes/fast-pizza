1ad12b6f97506f3067f1a1a78e71a6cda1aa0132
ezgikardes <ezgikardes@gmail.com>
2026-01-04 21:18:05 +0100

    feat: update CartOverview to display dynamic cart quantity and price from Redux state

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,20 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getTotalcartPrice, getTotalcartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
+
+  const totalCartQuantity = useSelector(getTotalcartQuantity)
+  const totalCartPrice = useSelector(getTotalcartPrice)
+
+  if (!totalCartQuantity) return null;
+
   return (
     <div className="flex items-center justify-between px-4 py-4 text-sm uppercase text-stone-200 bg-stone-800 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold sm:space-x text-stone-300">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
