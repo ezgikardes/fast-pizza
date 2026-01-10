5900f75e8d74c949f95216bcb638ffee662c9d19
ezgikardes <ezgikardes@gmail.com>
2026-01-10 18:26:03 +0100

    feat: enhance cart functionality with item deletion and quantity updates in Cart and MenuItem components

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,7 +7,8 @@ function Button({ children, disabled, to, type, onClick }) {
 
     const styles = {
         primary: base + ' px-4 py-3 md:px-6 md:py-4',
-        small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+        small: base + ' px-4 py-2 md:px-3.5 md:py-2 text-xs',
+        round: base + ' px-2.5 py-1 md:px-5 md:py-2.5 text-sm',
         secondary: 'inline-block text-sm font-semibold tracking-wide uppercase transition-colors duration-300 rounded-full text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:outline-none focus:text-stone-800 focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed border-2 border-stone-300 px-4 py-2.5 md:px-6 md:py-3.5'
     }
 
