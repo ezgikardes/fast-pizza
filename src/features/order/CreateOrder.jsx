d5fcf21d6a78dbaec66f4e6f21cc4bbd2e9af00e
ezgikardes <ezgikardes@gmail.com>
2026-01-11 13:38:10 +0100

    feat: integrate address fetching in CreateOrder component and update userSlice for geolocation handling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalcartPrice } from '../cart/cartSlice'
 import EmptyCart from '../cart/EmptyCart'
 import store from '../../store'
 import { formatCurrency } from "../../utils/helpers";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -22,6 +23,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting"
 
   const formErrors = useActionData();
+  const dispatch = useDispatch();
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = useSelector(getCart);
@@ -35,6 +37,8 @@ function CreateOrder() {
   return (
     <div className="py-6 px-4">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get position</button>
 
       <Form method='POST' action="/order/new">
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
